f715acc84d49b98d92e14d48497e2d1ef76793c2
maulanaysf002 <maulanaysf.dev@gmail.com>
2025-06-01 23:49:40 +0700

    golang basic (operator)

---
+++ Go/basic/operator.go
@@ -0,0 +1,22 @@
+package basic
+
+// Aritmatika
+// (+, - , *, /, %)
+var OperatorTambah int = 5 + 5
+var OperatorKurang int = 5 - 4
+var OperatorKali int = 5 * 5
+var OperatorBagi int = 5 / 1
+var OperatorModulus int = 5 % 2
+var PerhitunganSederhana = (5 * 5) + 20 - 30
+
+// Perbandingan
+// (>, >=, <, <=, ==, !=)
+
+var LebihBesarDari bool = 5 > 9            // false
+var LebihKecilDari bool = 5 < 9            // false
+var LebihBesarDariSamadengan bool = 5 >= 5 // true
+var LebihKecilDariSamadengan bool = 5 <= 9 // true
+var Samadengan = "mangga" == "mangga"      //true
+var TidakSamadengan = "mangga" != "jeruk"  //true
+
+// Logika
--- Go/main.go
+++ Go/main.go
@@ -9,20 +9,41 @@ func main() {
 
 	fmt.Println("ini bagian main")
 	basic.Basic_sintax()
+	fmt.Println("-------------------------------------------------")
 
 	// VARIABLE
 	// fmt.Println(basic.Variable_func())
+	fmt.Println("Hasil dari Belajar Variable")
 	fmt.Printf("Bulan ini = %s\n", basic.ThisMonth)
 	fmt.Printf("Hewan 1: %s, hewan 2: %s\n", basic.Animal1, basic.Animal2)
 	fmt.Println("-------------------------------------------------")
 
 	// TIPE DATA
+	fmt.Println("Hasil Dari Belajar Tipe Data")
 	fmt.Println(basic.Fruit)
 	fmt.Println(basic.Angka1)
 	fmt.Println(basic.Angka2)
 	fmt.Printf("Nilai decimal = %.4f\n", basic.DecimalNumber) // %f (untuk format angka decimal)
 	fmt.Printf("Hadir? %t\n", basic.Hadir)                    // %t (untuk format data boolean)
 	fmt.Printf("Buah = %s\n", basic.Fruit)                    // %s (untuk format tipe data string)
+	fmt.Println("-------------------------------------------------")
+
+	// OPERATOR
+	fmt.Println("Hasil Dari Belajar Operator")
+	fmt.Println("----- operator aritmatika -----")
+	fmt.Printf("Hasil Operasi Tambah: %d\n", basic.OperatorTambah)                      // %d (untuk format number)
+	fmt.Printf("Hasil Operasi Kurang: %d\n", basic.OperatorKurang)                      // %d (untuk format number)
+	fmt.Printf("Hasil Operasi Kali: %d\n", basic.OperatorKali)                          // %d (untuk format number)
+	fmt.Printf("Hasil Operasi Bagi: %d\n", basic.OperatorBagi)                          // %d (untuk format number)
+	fmt.Printf("Hasil Operasi Modulus: %d\n", basic.OperatorModulus)                    // %d (untuk format number)
+	fmt.Printf("Hasil Operasi Perhitungan Sederhana: %d\n", basic.PerhitunganSederhana) // %d (untuk format number)
+	fmt.Println("----- operator perbandingan -----")
+	fmt.Printf("Hasil Operator Lebih Besar Dari: %t\n", basic.LebihBesarDari)
+	fmt.Printf("Hasil Operator Lebih Kecil Dari: %t\n", basic.LebihKecilDari)
+	fmt.Printf("Hasil Operator Lebih Besar Dari Sama Dengan: %t\n", basic.LebihBesarDariSamadengan)
+	fmt.Printf("Hasil Operator Lebih Kecil Dari Sama Dengan: %t\n", basic.LebihKecilDariSamadengan)
+	fmt.Printf("Hasil Operator Sama Dengan: %t\n", basic.Samadengan)
+	fmt.Printf("Hasil Operator Tidak Sama Dengan: %t\n", basic.TidakSamadengan)
 	fmt.Println("-------------------------------------------------")
 
 }
